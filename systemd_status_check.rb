cce6e46708200e6c66f361927afecd3f5832cc06
Ladislav Slezák <lslezak@suse.cz>
2017-09-23 21:58:56 +0200

    Use Rubocop

--- systemd_status_check.rb
+++ systemd_status_check.rb
@@ -4,13 +4,15 @@
 require "diff/lcs"
 # Rainbow is used by Rubocop, it is already present in the YaST Docker image
 require "rainbow"
+
 require "yaml"
+require "English"
 
 # run systemctl and get the help about the defined states
 # @return [String]
 def systemd_help
   help = `systemctl --state=help`
-  raise "Cannot read systemd states help" unless $?.success?
+  raise "Cannot read systemd states help" unless $CHILD_STATUS.success?
   help
 end
 
@@ -75,12 +77,13 @@ end
 def compare_states(expected, current)
   compared_groups = expected.map do |name, states|
 
+    # sort the states to accept different order
     known_states = states.sort
     current_states = current[name].sort
 
     if known_states == current_states
       puts Rainbow("Found expected states in the #{name.inspect} group:").green
-      puts known_states.map{|s| "  #{s}"}.join("\n")
+      puts known_states.map { |s| "  #{s}" }.join("\n")
       puts
     else
       print_diff(name, known_states, current_states)
@@ -91,7 +94,6 @@ def compare_states(expected, current)
 
   compared_groups.all?
 end
-
 
 # get the current states
 current = parse_status_help(systemd_help)
@@ -107,4 +109,3 @@ else
   puts Rainbow("Check failed!").red
   exit 1
 end
-
